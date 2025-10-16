ae0e1c2266e690e05159746b6ab786610e03de9d
bpuglieseal24dw <bpuglieseal24dw@ikzubirimanteo.com>
2025-10-16 11:06:51 +0200

    feat: creando header de la aplicacion

--- dentanage-client/src/components/shared/header.tsx
+++ dentanage-client/src/components/shared/header.tsx
@@ -5,14 +5,28 @@ import {Button} from '../ui/button'
 export const Header: FC<object> = () => {
   return (
     <header>
-      <div>
-        <Button variant="link">
-          <NavLink to="/clients">Clients</NavLink>
-        </Button>
-        <Button variant="link">
-          <NavLink to="/dentists">Dentists</NavLink>
-        </Button>
-      </div>
+      <nav>
+        <ul className="flex flex-row justify-center pt-6 w-3/4 mx-auto gap-4">
+          <li>
+            <Button
+              size="lg"
+              variant="outline"
+              asChild
+            >
+              <NavLink to="/clients">Clients</NavLink>
+            </Button>
+          </li>
+          <li>
+            <Button
+              size="lg"
+              variant="outline"
+              asChild
+            >
+              <NavLink to="/dentists">Dentists</NavLink>
+            </Button>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
--- dentanage-client/src/main.tsx
+++ dentanage-client/src/main.tsx
@@ -4,9 +4,11 @@ import App from './App.tsx'
 
 // Providers
 import {BrowserRouter, Routes, Route} from 'react-router'
+import {Header} from './components/shared/header.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
+    <Header />
     <Routes>
       <Route
         path="dentists"
