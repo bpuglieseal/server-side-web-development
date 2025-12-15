13f71ed4adf479c4deb86150c51870ad34024b4d
bpuglieseal24dw <bpuglieseal24dw@ikzubirimanteo.com>
2025-12-15 10:06:48 +0100

    creando modelo "Car", migracion y seeder de Parking Manager I

---
+++ parking-manager/app/Models/Car.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+    /** @use HasFactory<\Database\Factories\CarFactory> */
+    use HasFactory;
+    use HasUuids;
+
+    protected $fillable = ["plate", "brand", "model"];
+    public $timestamps = false;
+}
---
+++ parking-manager/database/factories/CarFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Dotenv\Util\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Car>
+ */
+class CarFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => (string) \Illuminate\Support\Str::uuid(),
+            'plate' => $this->faker->bothify('???-####'),
+            'brand' => $this->faker->company(),
+            'model' => $this->faker->word(),
+        ];
+    }
+}
